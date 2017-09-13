ec16f4e819a612fb6525b750b3810934818f271c
Daniele122898 <d.palbanese@gmail.com>
2017-09-14 00:46:31 +0200

    Initial start. Added Connection as well as first open command: GetTypes

---
+++ Weeb.net/Weeb.net/WeebClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Weeb.net.Data;
+
+namespace Weeb.net
+{
+    public class WeebClient
+    {
+        private WeebHttp _weebHttp;
+        internal const string WeebNetVersion = "1.0.0-alpha.1";
+
+        //https://docs.weeb.sh/
+        
+        public async void Authenticate(string token)
+        {
+            _weebHttp = new WeebHttp(token);
+            var welcome = await _weebHttp.Welcome();
+            Console.WriteLine($"Connected to Weeb Api version: {welcome.Version}\nUsing Weeb.net wrapper {WeebNetVersion}");
+        }
+
+        public async Task<TypesData> GetTypesAsync(bool hidden = false)
+        {
+            return await _weebHttp.GetTypes(hidden);
+        }
+    }
+}
---
+++ Weeb.net/Weeb.net/WeebHttp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Weeb.net.Data;
+
+namespace Weeb.net
+{
+    internal class WeebHttp
+    {
+        private const string BaseUrl = "https://api.weeb.sh/";
+        private string _token;
+        private readonly HttpClient _client;
+
+        /// <summary>
+        /// Initialize the Weeb.api wrapper using your API token
+        /// </summary>
+        /// <param name="token">Your weeb api token</param>
+        public WeebHttp(string token)
+        {
+            _token = token;
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = true
+            };
+            _client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(BaseUrl),
+            };
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        internal async Task<WelcomeData> Welcome()
+        {
+            //get result
+            var result = await _client.GetStringAsync(Endpoints.Images);
+            //convert from json to usable c# objects
+            return JsonConvert.DeserializeObject<WelcomeData>(result);
+            
+        }
+
+        internal async Task<TypesData> GetTypes(bool hidden)
+        {
+            var result = await _client.GetStringAsync(Endpoints.Types);
+            return JsonConvert.DeserializeObject<TypesData>(result);
+        }
+    }
+}
